cad962272f0c52a7de770d3bed355eb1e085487c
Alice Cecile <alice.i.cecile@gmail.com>
2022-01-02 22:13:54 -0500

    Optionally implement the thiserror::Error trait
    Closes #8

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //! One particularly useful quirk is that elements are not recompacted upon removal: this can be very useful when replacing elements in a set or using the indexes that the elements are stored at in a semantic fashion.
 //! Iteration order is guaranteed to be stable, on a first-in-first-out basis.
 
-#![no_std]
+#![cfg_attr(not(feature = "thiserror_trait"), no_std)]
 #![deny(missing_docs)]
 
 use core::fmt::{Debug, Formatter, Result};
@@ -16,15 +16,26 @@ pub use map::{PetitMap, SuccesfulMapInsertion};
 mod set;
 pub use set::{PetitSet, SuccesfulSetInsertion};
 
+#[cfg(feature = "thiserror_trait")]
+use thiserror::Error;
+
 /// An error returned when attempting to insert into a full [`PetitSet`] or [`PetitMap`].
 ///
 /// It contains the element that could not be inserted.
 #[derive(PartialEq, Eq, Clone, Copy)]
+#[cfg_attr(feature = "thiserror_trait", derive(Error))]
 pub struct CapacityError<T>(pub T);
 
 impl<T> Debug for CapacityError<T> {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         f.debug_struct("A `PetitSet` or `PetitMap` has overflowed.")
             .finish()
+    }
+}
+
+#[cfg(feature = "thiserror_trait")]
+impl<T> std::fmt::Display for CapacityError<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        self::Debug::fmt(self, f)
     }
 }
