2d9401152f95b65e1e613d7fbac04c71b0186a60
Alice Cecile <alice.i.cecile@gmail.com>
2022-01-21 16:23:28 -0500

    Forbid unsafe code

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@
 
 #![cfg_attr(not(feature = "thiserror_trait"), no_std)]
 #![forbid(missing_docs)]
+#![forbid(unsafe_code)]
 #![cfg_attr(feature = "set_algebra", allow(incomplete_features))]
 #![cfg_attr(feature = "set_algebra", feature(generic_const_exprs))]
 
