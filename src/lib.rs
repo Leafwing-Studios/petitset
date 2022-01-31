475f5ecfa62b462484250657d896bcb67757e3f0
Alice I. Cecile <alice.i.cecile@gmail.com>
2022-01-30 22:20:15 -0500

    Split contributing to seperate file

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,8 @@
-//! Array-backed ordered set and map data structures in Rust, optimized for stack-allocated storage of a tiny number of elements with a fixed cap.
-//! Your elements only need `Eq`, and this crate is`no_std` compatible!
-//!
-//! This crate is designed to be used in performance-sensitive contexts with a small number of elements, where iteration is more common than look-ups and you don't mind a fixed size.
-//! One particularly useful quirk is that elements are not recompacted upon removal: this can be very useful when replacing elements in a set or using the indexes that the elements are stored at in a semantic fashion.
-//! Iteration order is guaranteed to be stable, on a first-in-first-out basis.
-
 #![cfg_attr(not(feature = "thiserror_trait"), no_std)]
 #![forbid(missing_docs)]
 #![forbid(unsafe_code)]
+#![warn(clippy::doc_markdown)]
+#![doc = include_str!("../README.md")]
 #![cfg_attr(feature = "set_algebra", allow(incomplete_features))]
 #![cfg_attr(feature = "set_algebra", feature(generic_const_exprs))]
 
